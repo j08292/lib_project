9d48c87130d6a063b4df2d4e272b200481fdb65d
user2 <user2@user2-PC>
2016-10-06 18:18:29 +0900

    ddd

--- lib/src/main/java/kr/spring/book/controller/ReserveCancelAjaxController.java
+++ lib/src/main/java/kr/spring/book/controller/ReserveCancelAjaxController.java
@@ -59,7 +59,7 @@ public class ReserveCancelAjaxController {
 							log.debug("rent_num : " + rent_num);
 						}
 						
-						if((rentCommand.get(i).getRent_num() == rent_num) && rentCommand.get(i).getRent_status() == 2){
+						if((rentCommand.get(i).getRent_num() == rent_num) && rentCommand.get(i).getRent_status() == 2){ 
 							
 							if (log.isDebugEnabled()) {
 								log.debug("i : " + i);
@@ -70,10 +70,10 @@ public class ReserveCancelAjaxController {
 							bookRentService.updateStatus(bookRentCommand);
 							map.put("result", "success");
 							break;
-						}else{
+						}/*else{
 							map.put("result", "fail");
 							break;
-						}
+						}*/
 					}
 				}
 			} else if (userId == null) {
